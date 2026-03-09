d3b8d4296b2fdd6c57c2099c4c8e1bae9d5bbde6
Keyner Arismendy <keinerf588@gmail.com>
2026-03-09 15:43:50 -0500

    Configuracion principal de la aplicacion Express

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import pqrsRoutes from './routes/pqrsRoutes.js';
+
+const app = express()
+const port = 3000
+
+app.use(express.json());
+app.use('/api', pqrsRoutes);
+
+app.listen(port, ()=>{
+    console.log(`Servidor iniciado en el puerto :${port}`);
+})
