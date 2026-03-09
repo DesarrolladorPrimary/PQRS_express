f301121c8b8254543956948b03600b211354bc9e
Keyner Arismendy <keinerf588@gmail.com>
2026-03-09 16:29:50 -0500

    Uso de urluncoded para tomar datos por postman

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import pqrsRoutes from './routes/pqrsRoutes.js';
 const app = express()
 const port = 3000
 
-app.use(express.json());
+app.use(express.urlencoded());
 app.use('/api', pqrsRoutes);
 
 app.listen(port, ()=>{
